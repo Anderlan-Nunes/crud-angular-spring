868a9c9aad1b5d35e5f84e6c5c5e7e6bef184e4b
term02 <ter02@gmail.com>
2025-04-02 19:53:32 -0300

    commit

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+
+import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { Course } from './models/course';
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+  courses : Course[] = [];
+  displayedColumns: string[] = ['name', 'category'];
+  
 
 }
