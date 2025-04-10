fab1246ec963273fa192c3b62b95ba40ef931ed8
Anderlan <anderlan.dev@gmail.com>
2025-04-10 12:25:21 -0300

    feat: criar loading-spinner

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { Course } from './models/course';
 import { CoursesService } from './services/courses.service';
-import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule, MatCardModule, MatToolbarModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule,
+    CommonModule
+  ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
 
   displayedColumns: string[] = ['name', 'category'];
-  courses: Observable<Course[]>
+  courses$: Observable<Course[]>
 
   constructor(private coursesService: CoursesService) {
-    this.courses = this.coursesService.listCourses();
+    this.courses$ = this.coursesService.listCourses();
 
   }
   ngOnInit() {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../models/course';
-import { first, Observable, tap } from 'rxjs';
+import { delay, first, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +13,9 @@ export class CoursesService {
   constructor(private httpClient : HttpClient) { }
 
   listCourses() : Observable<Course[]> {
-    return this.httpClient.get<Course[]>(this.API).pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe( 
       first(), // finaliza a inscrição assim quando obter a primeira resposta que o servidor me enviar, (usa quando nao eh um websocket)
+      delay(20000),
       tap(courses => console.log(courses)) // tap é um operador que permite executar uma função com o resultado do observable, sem alterar o resultado do observable usa-se para fazer debug
     );  
   }
