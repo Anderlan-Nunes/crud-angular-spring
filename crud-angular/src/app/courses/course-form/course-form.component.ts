5dac6d82684ea4c6d4891fe866c5c752465c0c34
pc-kim <anderlan.dev@gmail.com>
2025-05-15 22:52:00 -0300

    feat: Implementa formulário de criação de curso com Angular Material
    Este commit adiciona o formulário para criar novos cursos, utilizando
    ReactiveFormsModule para o gerenciamento do formulário e Angular Material
    para a estilização dos campos e layout.

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,12 +1,31 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-course-form',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatToolbarModule,
+    ReactiveFormsModule
+  ],
+  standalone: true,
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseFormComponent {
+
+  form: FormGroup;
+  constructor(private readonly formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+
+    });
+  }
 
 }
