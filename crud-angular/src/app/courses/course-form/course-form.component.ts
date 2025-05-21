d77db05b035496681b431d25d8f73c60145f1f4d
pc-kim <anderlan.dev@gmail.com>
2025-05-20 22:09:43 -0300

    feat: Troca de formulário por select

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -6,10 +6,16 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+
+interface Category {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-course-form',
-  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatToolbarModule,
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatSelectModule,
     ReactiveFormsModule
   ],
   standalone: true,
@@ -19,12 +25,18 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 })
 export class CourseFormComponent {
 
+category: Category[] = [
+    {value: '0', viewValue: 'Full-stack'},
+    {value: '1', viewValue: 'Front-end'},
+    {value: '2', viewValue: 'Back-end'},
+  ];
+
   form: FormGroup;
   constructor(private readonly formBuilder: FormBuilder) { 
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
-
+      // category: [this.category[0].value],
     });
   }
 
